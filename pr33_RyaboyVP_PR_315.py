01db1e24e84a01084925954d3e34298e41c2bc1a
VladR-natk <78097516+VladR-natk@users.noreply.github.com>
2022-02-26 18:08:23 +0700

    Add files via upload

--- pr33_RyaboyVP_PR_315.py
+++ pr33_RyaboyVP_PR_315.py
@@ -1,8 +1,8 @@
+from pickle import TRUE
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.codeinput import CodeInput
 from pygments.lexers import HtmlLexer
-
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scatter import Scatter
 
@@ -12,24 +12,25 @@ class myApp(App):
         fl=FloatLayout(size=(400,600))
         s.add_widget(fl)
         fl.add_widget(Button(
-            text="Это первая кнопка",
-            font_size=25,
+            text="Это кнопка №1",
+            font_size=22,
             on_press=self.btn_press,
             background_color=[1,.5,0,1],
             background_normal='',
             size_hint=(.5, .25),
             pos=(300, 120)));
         fl.add_widget(Button(
-            text="Это вторая кнопка",
-            font_size=25,
+            text="Это кнопка №2",
+            font_size=22,
             on_press=self.btn_press,
             background_color=[1,2,0,2],
             size_hint=(.5, .25),
             pos=(300, 320)));
-        s.bind(center=fl.setter('center'))
         return s
     def btn_press(self, instance):
-        instance.text='Я нажата'
-
+        if instance.text == 'Это кнопка №1':
+            instance.text='Нажата кнопка №1'
+        elif instance.text=='Это кнопка №2':
+            instance.text='Нажата кнопка №2'
 if __name__=="__main__":
     myApp().run()
